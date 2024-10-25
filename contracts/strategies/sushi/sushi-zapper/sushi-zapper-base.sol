648991df4f0310ca0a0a5eac265101909a7e3990
abdulrafay-contrax <abdulrafay@contrax.finance>
2024-10-25 12:57:02 +0500

    chore: updated sushi zapper, added return values, events

--- contracts/strategies/sushi/sushi-zapper/sushi-zapper-base.sol
+++ contracts/strategies/sushi/sushi-zapper/sushi-zapper-base.sol
@@ -8,188 +8,238 @@ import "../../../lib/square-root.sol";
 import "../../../interfaces/weth.sol";
 import "../../../interfaces/vault.sol";
 
-abstract contract SushiZapperBase {
-    using SafeERC20 for IERC20;
-    using Address for address;
-    using SafeMath for uint256;
-    using SafeERC20 for IVault;
+abstract contract ZapperBase {
+  using SafeERC20 for IERC20;
+  using Address for address;
+  using SafeMath for uint256;
+  using SafeERC20 for IVault;
 
-    address public router;
+  address public router;
 
-    address public constant weth = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+  address public governance;
 
-    uint256 public constant minimumAmount = 1000;
+  // Define a mapping to store whether an address is whitelisted or not
+  mapping(address => bool) public whitelistedVaults;
 
-    constructor(address _router) {
-        // Safety checks to ensure WETH token address
-        WETH(weth).deposit{value: 0}();
-        WETH(weth).withdraw(0);
-        router = _router;
-    }
+  address public constant weth = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
 
-    receive() external payable {
-        assert(msg.sender == weth);
-    }
+  uint256 public constant minimumAmount = 1000;
 
-    function _getSwapAmount(uint256 investmentA, uint256 reserveA, uint256 reserveB) public view virtual returns (uint256 swapAmount);
+  constructor(address _router, address _governance) {
+    // Safety checks to ensure WETH token address
+    WETH(weth).deposit{value: 0}();
+    WETH(weth).withdraw(0);
+    router = _router;
+    governance = _governance;
+  }
 
-    //returns DUST
-    function _returnAssets(address[] memory tokens) internal {
-        uint256 balance;
-        for (uint256 i; i < tokens.length; i++) {
-            balance = IERC20(tokens[i]).balanceOf(address(this));
-            if (balance > 0) {
-                if (tokens[i] == weth) {
-                    WETH(weth).withdraw(balance);
-                    (bool success, ) = msg.sender.call{value: balance}(
-                        new bytes(0)
-                    );
-                    require(success, "ETH transfer failed");
-                } else {
-                    IERC20(tokens[i]).safeTransfer(msg.sender, balance);
-                }
-            }
+  event Deposit(address indexed recipient, uint256 amountIn);
+  event Withdraw(address indexed recipient, uint256 amountOut);
+
+  receive() external payable {
+    assert(msg.sender == weth);
+  }
+
+  // **** Modifiers **** //
+
+  // Modifier to restrict access to whitelisted vaults only
+  modifier onlyWhitelistedVaults(address vault) {
+    require(whitelistedVaults[vault], "Vault is not whitelisted");
+    _;
+  }
+
+  modifier onlyGovernance() {
+    require(msg.sender == governance, "Caller is not the governance");
+    _;
+  }
+
+  // Function to add a vault to the whitelist
+  function addToWhitelist(address _vault) external onlyGovernance {
+    whitelistedVaults[_vault] = true;
+  }
+
+  // Function to remove a vault from the whitelist
+  function removeFromWhitelist(address _vault) external onlyGovernance {
+    whitelistedVaults[_vault] = false;
+  }
+
+  function _getSwapAmount(
+    uint256 investmentA,
+    uint256 reserveA,
+    uint256 reserveB
+  ) public view virtual returns (uint256 swapAmount);
+
+  //returns DUST
+  function _returnAssets(address[] memory tokens) internal {
+    uint256 balance;
+    for (uint256 i; i < tokens.length; i++) {
+      balance = IERC20(tokens[i]).balanceOf(address(this));
+      if (balance > 0) {
+        if (tokens[i] == weth) {
+          WETH(weth).withdraw(balance);
+          (bool success, ) = msg.sender.call{value: balance}(new bytes(0));
+          require(success, "ETH transfer failed");
+        } else {
+          IERC20(tokens[i]).safeTransfer(msg.sender, balance);
         }
+      }
     }
+  }
 
-    function _swapAndStake(address vault_addr, uint256 tokenAmountOutMin, address tokenIn) public virtual;
+  function _swapAndStake(
+    address vault_addr,
+    uint256 tokenAmountOutMin,
+    address tokenIn
+  ) public virtual returns (uint256);
 
-    function zapInETH(address vault_addr, uint256 tokenAmountOutMin, address tokenIn) external payable{
-        require(msg.value >= minimumAmount, "Insignificant input amount");
+  function zapInETH(
+    address vault_addr,
+    uint256 tokenAmountOutMin,
+    address tokenIn
+  ) external payable onlyWhitelistedVaults(vault_addr) returns (uint256 vaultBalance) {
+    require(msg.value >= minimumAmount, "Insignificant input amount");
 
-        WETH(weth).deposit{value: msg.value}();
+    WETH(weth).deposit{value: msg.value}();
 
-        // allows us to zapIn if eth isn't part of the original pair
-        if (tokenIn != weth){
-            uint256 _amount = IERC20(weth).balanceOf(address(this));
+    // allows us to zapIn if eth isn't part of the original pair
+    if (tokenIn != weth) {
+      uint256 _amount = IERC20(weth).balanceOf(address(this));
 
-            (, IUniswapV2Pair pair) = _getVaultPair(vault_addr);
+      (, IUniswapV2Pair pair) = _getVaultPair(vault_addr);
 
-            (uint256 reserveA, uint256 reserveB, ) = pair.getReserves();
-            require(reserveA > minimumAmount && reserveB > minimumAmount, "Liquidity pair reserves too low");
+      (uint256 reserveA, uint256 reserveB, ) = pair.getReserves();
+      require(reserveA > minimumAmount && reserveB > minimumAmount, "Liquidity pair reserves too low");
 
-            bool isInputA = pair.token0() == tokenIn;
-            require(isInputA || pair.token1() == tokenIn, "Input token not present in liquidity pair");
+      bool isInputA = pair.token0() == tokenIn;
+      require(isInputA || pair.token1() == tokenIn, "Input token not present in liquidity pair");
 
-            address[] memory path = new address[](2);
-            path[0] = weth;
-            path[1] = tokenIn;
+      address[] memory path = new address[](2);
+      path[0] = weth;
+      path[1] = tokenIn;
 
-            _approveTokenIfNeeded(path[0], address(router));
-            UniswapRouterV2(router).swapExactTokensForTokens(
-                _amount,
-                tokenAmountOutMin,
-                path,
-                address(this),
-                block.timestamp
-            );
-            
-            _swapAndStake(vault_addr, tokenAmountOutMin, tokenIn);
-        }else{
-            _swapAndStake(vault_addr, tokenAmountOutMin, tokenIn);
-        }
+      _approveTokenIfNeeded(path[0], address(router));
+      UniswapRouterV2(router).swapExactTokensForTokens(
+        _amount,
+        tokenAmountOutMin,
+        path,
+        address(this),
+        block.timestamp
+      );
+
+      vaultBalance = _swapAndStake(vault_addr, tokenAmountOutMin, tokenIn);
+    } else {
+      vaultBalance = _swapAndStake(vault_addr, tokenAmountOutMin, tokenIn);
     }
+  }
 
-    // transfers tokens from msg.sender to this contract 
-    function zapIn(address vault_addr, uint256 tokenAmountOutMin, address tokenIn, uint256 tokenInAmount) external {
-        require(tokenInAmount >= minimumAmount, "Insignificant input amount");
-        require(IERC20(tokenIn).allowance(msg.sender, address(this)) >= tokenInAmount, "Input token is not approved");
+  // transfers tokens from msg.sender to this contract
+  function zapIn(
+    address vault_addr,
+    uint256 tokenAmountOutMin,
+    address tokenIn,
+    uint256 tokenInAmount
+  ) external onlyWhitelistedVaults(vault_addr) returns (uint256 vaultBalance) {
+    require(tokenInAmount >= minimumAmount, "Insignificant input amount");
+    require(IERC20(tokenIn).allowance(msg.sender, address(this)) >= tokenInAmount, "Input token is not approved");
 
-        // transfer token 
-        IERC20(tokenIn).safeTransferFrom(
-            msg.sender,
-            address(this),
-            tokenInAmount
+    // transfer token
+    IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), tokenInAmount);
+
+    (, IUniswapV2Pair pair) = _getVaultPair(vault_addr);
+
+    if (tokenIn != pair.token0() && tokenIn != pair.token1()) {
+      address desiredToken = pair.token0();
+
+      if (tokenIn != weth && desiredToken != weth) {
+        // execute swap from tokenin to desired token
+        uint256 _amount = IERC20(tokenIn).balanceOf(address(this));
+
+        address[] memory path = new address[](3);
+        path[0] = tokenIn;
+        path[1] = weth;
+        path[2] = desiredToken;
+
+        _approveTokenIfNeeded(path[0], address(router));
+        UniswapRouterV2(router).swapExactTokensForTokens(
+          _amount,
+          tokenAmountOutMin,
+          path,
+          address(this),
+          block.timestamp
         );
+      } else {
+        // execute swap from tokenin to desired token
+        uint256 _amount = IERC20(tokenIn).balanceOf(address(this));
 
-        (, IUniswapV2Pair pair) = _getVaultPair(vault_addr);
+        address[] memory path = new address[](2);
+        path[0] = tokenIn;
+        path[1] = desiredToken;
 
-        if(tokenIn != pair.token0() && tokenIn != pair.token1()){
-            (address desiredToken) = pair.token0();
+        _approveTokenIfNeeded(path[0], address(router));
+        UniswapRouterV2(router).swapExactTokensForTokens(
+          _amount,
+          tokenAmountOutMin,
+          path,
+          address(this),
+          block.timestamp
+        );
+      }
+      vaultBalance = _swapAndStake(vault_addr, tokenAmountOutMin, desiredToken);
+    } else {
+      vaultBalance = _swapAndStake(vault_addr, tokenAmountOutMin, tokenIn);
+    }
+  }
 
-            if(tokenIn != weth && desiredToken != weth){
-                // execute swap from tokenin to desired token
-                uint256 _amount = IERC20(tokenIn).balanceOf(address(this));
+  function zapOutAndSwap(
+    address vault_addr,
+    uint256 withdrawAmount,
+    address desiredToken,
+    uint256 desiredTokenOutMin
+  ) public virtual returns (uint256 tokenBalance);
 
-                address[] memory path = new address[](3);
-                path[0] = tokenIn;
-                path[1] = weth;
-                path[2] = desiredToken;
-        
-                _approveTokenIfNeeded(path[0], address(router));
-                UniswapRouterV2(router).swapExactTokensForTokens(
-                    _amount,
-                    tokenAmountOutMin,
-                    path,
-                    address(this),
-                    block.timestamp
-                );
-            }else {
-                // execute swap from tokenin to desired token
-                uint256 _amount = IERC20(tokenIn).balanceOf(address(this));
+  function zapOutAndSwapEth(
+    address vault,
+    uint256 withdrawAmount,
+    uint256 desiredTokenOutMin
+  ) public virtual returns (uint256 ethBalance);
 
-                address[] memory path = new address[](2);
-                path[0] = tokenIn;
-                path[1] = desiredToken;
-        
-                _approveTokenIfNeeded(path[0], address(router));
-                UniswapRouterV2(router).swapExactTokensForTokens(
-                    _amount,
-                    tokenAmountOutMin,
-                    path,
-                    address(this),
-                    block.timestamp
-                );
-            }
-            _swapAndStake(vault_addr, tokenAmountOutMin, desiredToken);
-        }else{
-            _swapAndStake(vault_addr, tokenAmountOutMin, tokenIn);
-        }
-   
+  function _removeLiquidity(address pair, address to) internal {
+    IERC20(pair).safeTransfer(pair, IERC20(pair).balanceOf(address(this)));
+    (uint256 amount0, uint256 amount1) = IUniswapV2Pair(pair).burn(to);
+
+    require(amount0 >= minimumAmount, "Router: INSUFFICIENT_A_AMOUNT");
+    require(amount1 >= minimumAmount, "Router: INSUFFICIENT_B_AMOUNT");
+  }
+
+  function _getVaultPair(address vault_addr) internal view returns (IVault vault, IUniswapV2Pair pair) {
+    vault = IVault(vault_addr);
+    pair = IUniswapV2Pair(vault.token());
+
+    require(pair.factory() == IUniswapV2Pair(router).factory(), "Incompatible liquidity pair factory");
+  }
+
+  function _approveTokenIfNeeded(address token, address spender) internal {
+    if (IERC20(token).allowance(address(this), spender) == 0) {
+      IERC20(token).safeApprove(spender, type(uint256).max);
+    }
+  }
+
+  function zapOut(address vault_addr, uint256 withdrawAmount) external {
+    (IVault vault, IUniswapV2Pair pair) = _getVaultPair(vault_addr);
+
+    IERC20(vault_addr).safeTransferFrom(msg.sender, address(this), withdrawAmount);
+    vault.withdraw(withdrawAmount);
+
+    if (pair.token0() != weth && pair.token1() != weth) {
+      return _removeLiquidity(address(pair), msg.sender);
     }
 
-    function zapOutAndSwap(address vault_addr, uint256 withdrawAmount, address desiredToken, uint256 desiredTokenOutMin) public virtual;
+    _removeLiquidity(address(pair), address(this));
 
-    function zapOutAndSwapEth(address vault, uint256 withdrawAmount, uint256 desiredTokenOutMin) public virtual;
+    address[] memory tokens = new address[](2);
+    tokens[0] = pair.token0();
+    tokens[1] = pair.token1();
 
-    function _removeLiquidity(address pair, address to) internal {
-        IERC20(pair).safeTransfer(pair, IERC20(pair).balanceOf(address(this)));
-        (uint256 amount0, uint256 amount1) = IUniswapV2Pair(pair).burn(to);
-
-        require(amount0 >= minimumAmount, "Router: INSUFFICIENT_A_AMOUNT");
-        require(amount1 >= minimumAmount, "Router: INSUFFICIENT_B_AMOUNT");
-    }
- 
-    function _getVaultPair(address vault_addr) internal view returns (IVault vault, IUniswapV2Pair pair){
-        vault = IVault(vault_addr);
-        pair = IUniswapV2Pair(vault.token());
-
-        require(pair.factory() == IUniswapV2Pair(router).factory(), "Incompatible liquidity pair factory");
-    }
-
-    function _approveTokenIfNeeded(address token, address spender) internal {
-        if (IERC20(token).allowance(address(this), spender) == 0) {
-            IERC20(token).safeApprove(spender, type(uint256).max);
-        }
-    }
-
-    function zapOut(address vault_addr, uint256 withdrawAmount) external {
-        (IVault vault, IUniswapV2Pair pair) = _getVaultPair(vault_addr);
-
-        IERC20(vault_addr).safeTransferFrom(msg.sender, address(this), withdrawAmount);
-        vault.withdraw(withdrawAmount);
-
-        if (pair.token0() != weth && pair.token1() != weth) {
-            return _removeLiquidity(address(pair), msg.sender);
-        }
-
-
-        _removeLiquidity(address(pair), address(this));
-
-        address[] memory tokens = new address[](2);
-        tokens[0] = pair.token0();
-        tokens[1] = pair.token1();
-
-        _returnAssets(tokens);
-    }
+    _returnAssets(tokens);
+  }
 }
---
+++ scripts/sushi/deploy.ts
@@ -0,0 +1,124 @@
+import hre, { ethers } from "hardhat";
+
+const sleep = async (s: number) => {
+  for (let i = s; i > 0; i--) {
+    process.stdout.write(`\r \\ ${i} waiting..`);
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    process.stdout.write(`\r | ${i} waiting..`);
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    process.stdout.write(`\r / ${i} waiting..`);
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    process.stdout.write(`\r - ${i} waiting..`);
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    if (i === 1) process.stdout.clearLine(0);
+  }
+};
+
+const verify = async (
+  contractAddress: string,
+  args: (string | number)[] = [],
+  name?: string,
+  wait: number = 20,
+  contractPath?: string
+) => {
+  try {
+    await hre.run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+    return true;
+  } catch (e) {
+    if (
+      String(e).indexOf(`${contractAddress} has no bytecode`) !== -1 ||
+      String(e).indexOf(`${contractAddress} does not have bytecode`) !== -1
+    ) {
+      console.log(`Verification failed, waiting ${wait} seconds for etherscan to pick the deployed contract`);
+      await sleep(wait);
+    }
+
+    try {
+      await hre.run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+        contract: contractPath,
+      });
+      return true;
+    } catch (e) {
+      if (String(e).indexOf("Already Verified") !== -1 || String(e).indexOf("Already verified") !== -1) {
+        console.log(name ?? contractAddress, "is already verified!");
+        return true;
+      } else {
+        console.log(e);
+        return false;
+      }
+    }
+  }
+};
+
+const deploy = async (params: { name: string; args: any[]; verificationWait?: number; contractPath?: string }) => {
+  const contractFactory = await ethers.getContractFactory(params.name);
+  const contract = await contractFactory.deploy(...params.args);
+  await contract.deployed();
+  console.log(`${params.name}: ${contract.address}`);
+
+  if (hre.network.name === "localhost") return contract;
+
+  console.log("Verifying...");
+  await verify(contract.address, params.args, params.name, params.verificationWait, params.contractPath);
+
+  return contract;
+};
+
+const governance = "0xCb410A689A03E06de0a6247b13C13D14237DecC8";
+const timelock = governance;
+
+const sushiVaults = [
+  "0x3C0c76ceb491Cb0Bacb31F8e7dc6407A25FD87C0",
+  "0x3F9012f9bF3172c26B1B7246B8bc62148842B013",
+  "0xeb952db71c594299cEEe7c03C3AA26FE0fDBC8eb",
+  "0xdf9d86bC4765a9C64e85323A9408dbee0115d22E",
+  "0xb58004E106409B00b854aBBF8CCB8618673d9346",
+  "0xf8bDcf1Cf4134b2864cdbE685A8128F90ED0E16e",
+];
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  // const clipperController = await deploy({
+  //   name: "ClipperController",
+  //   args: [governance, governance, governance, governance, governance],
+  //   contractPath: "contracts/controllers/clipper-controller.sol:ClipperController",
+  // });
+
+  // await verify(
+  //   "0xa752C41Ca7De8B6852D9f6e17E224D166dCC456b",
+  //   [clipperAddress, governance, governance, controller],
+  //   "VaultClipperBase",
+  //   0,
+  //   "contracts/vaults/clipper/vault-clipper-base.sol:VaultClipperBase"
+  // );
+
+  // const clipperVault = await deploy({
+  //   name: "VaultClipperBase",
+  //   args: [clipperAddress, governance, governance, clipperController.address],
+  //   contractPath: "contracts/vaults/clipper/vault-clipper-base.sol:VaultClipperBase",
+  // });
+
+  // const clipperStrategy = await deploy({
+  //   name: "StrategyClipperBase",
+  //   args: [want, governance, governance, controller, governance],
+  //   contractPath: "contracts/strategies/clipper/strategy-clipper-base.sol:StrategyClipperBase",
+  // });
+
+  const sushiZapper = await deploy({
+    name: "ZapperSushi",
+    args: [governance, sushiVaults],
+    contractPath: "contracts/strategies/sushi/sushi-zapper/sushi-vault-zapper.sol:ZapperSushi",
+  });
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
